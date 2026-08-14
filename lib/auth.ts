524c819441a16082bb6d04ca9c4097aab8ed865f
seifselims <124066228+seifselims@users.noreply.github.com>
2026-08-14 16:46:03 +0300

    Initializing Drizzle and Better-Auth

---
+++ lib/auth.ts
@@ -0,0 +1,13 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "../src/db"; // your drizzle instance
+
+export const auth = betterAuth({
+    database: drizzleAdapter(db, {
+        provider: "pg", // or "mysql", "sqlite"
+    }),
+    emailAndPassword: { 
+        enabled: true, 
+        requireEmailVerification: true,
+      }, 
+});
